f9e27906d90194e60e4f4e31a58f9c858bbd96dd
Denys_Osypenko <cbble02@gmail.com>
2024-09-05 21:08:45 -0500

    cukes runner updated with json/cucumber.json

--- src/test/java/com/tor/runners/CukesRunner.java
+++ src/test/java/com/tor/runners/CukesRunner.java
@@ -9,6 +9,7 @@ import org.junit.runner.RunWith;
 @CucumberOptions(
 
             plugin = {
+                    "json:target/cucumber.json",
                     "html:target/cucumber-reports.html", // path target folder to store html report
                     "rerun:target/rerun.txt", // implements rerun and give the location where to store failed tests (create rerun.txt)
                     "me.jvt.cucumber.report.PrettyReports:target/cucumber" // reports mvn
@@ -17,7 +18,7 @@ import org.junit.runner.RunWith;
             features = "src/test/resources/features", // path feature files folder
             glue = "com/tor/step_defenitions", //path to step def
             dryRun = false, //run scenarios
-            tags = "@wip", // tag to choose particular scenario/method
+            tags = "", // tag to choose particular scenario/method
             publish = true // true - to get link with public html report
 )
 
